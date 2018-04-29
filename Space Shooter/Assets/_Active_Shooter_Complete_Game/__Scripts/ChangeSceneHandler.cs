e0f4911acce1e479ac8d3df464d5e7d47bbf6057
Nick <nickbrady253@gmail.com>
2018-04-29 03:22:29 -0600

    Added Start Screen

---
+++ Space Shooter/Assets/_Active_Shooter_Complete_Game/__Scripts/ChangeSceneHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneHandler : MonoBehaviour {
+
+	public void OnButtonClick(string Done_Main) 
+	{
+		SceneManager.LoadSceneAsync(Done_Main);
+		//Application.LoadLevel (Done_Main);
+	}
+}
